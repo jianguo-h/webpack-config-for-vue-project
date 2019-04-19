2aa45ffcb1fe85ea20db41866f0c82b4e1916d52
jianguo-h <1060684755@qq.com>
2019-04-19 23:57:19 +0800

    feat: 安装vue-router, vuex, element-ui等依赖增加模板

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,20 @@
 import Vue from 'vue';
 import App from './App';
+import axios from 'axios';
+import store from './store';
+import router from './router';
+import ElementUI from 'element-ui';
 
 if (module.hot) {
   module.hot.accept();
 }
 
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios;
+
 new Vue({
   el: '#app',
+  store,
+  router,
   render: h => h(App)
 });
