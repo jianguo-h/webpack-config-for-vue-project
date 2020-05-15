c7a6e66c45a7b548aa0d926d8e97986f22a461e6
jianguo-h <1060684755@qq.com>
2020-05-15 20:52:01 +0800

    chore: update webpack build files to typescript

--- build/webpack.prod.config.ts
+++ build/webpack.prod.config.ts
@@ -1,16 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
-const webpackBaseConfig = require('./webpack.base.config');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+import path from 'path';
+import webpack, { Configuration } from 'webpack';
+import webpackMerge from 'webpack-merge';
+import webpackBaseConfig from './webpack.base.config';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import AddAssetHtmlPlugin from 'add-asset-html-webpack-plugin';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
-const webpackProdConfig = webpackMerge(webpackBaseConfig, {
+const webpackProdConfig: Configuration = webpackMerge(webpackBaseConfig, {
   mode: 'production',
-  devtool: 'none',
+  devtool: false,
   module: {
     rules: [
       {
@@ -19,12 +19,12 @@ const webpackProdConfig = webpackMerge(webpackBaseConfig, {
           MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
-          'less-loader'
-        ]
+          'less-loader',
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.vue$/,
@@ -37,13 +37,13 @@ const webpackProdConfig = webpackMerge(webpackBaseConfig, {
               MiniCssExtractPlugin.loader,
               'css-loader',
               'postcss-loader',
-              'less-loader'
+              'less-loader',
             ],
-            css: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-          }
-        }
-      }
-    ]
+            css: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     // 每次打包前清除dist目录
@@ -51,28 +51,28 @@ const webpackProdConfig = webpackMerge(webpackBaseConfig, {
     // dllPlugin
     new webpack.DllReferencePlugin({
       context: __dirname,
-      manifest: require('../app/libs/libs-manifest.json')
+      manifest: require('../app/libs/libs-manifest.json'),
     }),
     // 将dllplugin生成的js自动注入到html中
     new AddAssetHtmlPlugin({
       publicPath: '/static/js/',
       filepath: path.resolve(__dirname, '../dll/*.js'),
-      outputPath: 'static/js'
+      outputPath: 'static/js',
     }),
     // 提取less和css
     new MiniCssExtractPlugin({
       filename: 'static/css/[name].[hash:8].css',
-      chunkFilename: 'static/css/[name].[chunkhash:8].css'
+      chunkFilename: 'static/css/[name].[chunkhash:8].css',
     }),
     // 压缩css
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
       cssProcessorOptions: {
         discardComments: {
-          removeAll: true
-        }
+          removeAll: true,
+        },
       },
-      canPrint: true
+      canPrint: true,
     }),
     // 压缩混淆js
     new UglifyJsPlugin({
@@ -80,16 +80,16 @@ const webpackProdConfig = webpackMerge(webpackBaseConfig, {
         warnings: false, // 删除警告
         compress: {
           drop_console: true, // 去除日志
-          drop_debugger: true // 去除debugger
+          drop_debugger: true, // 去除debugger
         },
         output: {
-          comments: false // 去除注释
-        }
+          comments: false, // 去除注释
+        },
       },
       cache: true, // 使用缓存
-      parallel: true // 开启多线程压缩
-    })
-  ]
+      parallel: true, // 开启多线程压缩
+    }),
+  ],
 });
 
-module.exports = webpackProdConfig;
+export default webpackProdConfig;
