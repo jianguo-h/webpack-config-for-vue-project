609042a7d64302914465159e87ccc892c27d356a
jianguo-h <1060684755@qq.com>
2020-05-15 19:49:25 +0800

    fix: eslint invalid error

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,48 +7,49 @@ module.exports = {
     'prettier',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'prettier/vue'
+    'prettier/vue',
   ],
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
     ecmaVersion: 2019,
     ecmaFeatures: {
-      legacyDecorators: true
-    }
+      legacyDecorators: true,
+    },
   },
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   rules: {
     'vue/script-indent': ['off', 2, { baseIndent: 1 }],
     '@typescript-eslint/no-var-requires': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-inferrable-types': ['error', {
-      ignoreParameters: true,
-      ignoreProperties: true
-    }]
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      {
+        ignoreParameters: true,
+        ignoreProperties: true,
+      },
+    ],
   },
   overrides: [
     {
       files: ['*.vue'],
       rules: {
         indent: 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off'
-      }
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
     },
     {
       files: ['*.js'],
       rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-        '@typescript-eslint/explicit-function-return-type': 'off'
-      }
-    }
-  ]
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
 };
