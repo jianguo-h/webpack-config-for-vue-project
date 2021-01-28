25c64653cc3d0189fb2325541c5fcff02b7dcd74
jianguo-h <1060684755@qq.com>
2021-01-28 10:17:14 +0800

    fix: css source map err

--- build/webpack.base.config.ts
+++ build/webpack.base.config.ts
@@ -54,7 +54,7 @@ const webpackBaseConfig: Configuration = {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
-              sourceMaps: !isProduction,
+              sourceMap: !isProduction,
             },
           },
           'postcss-loader',
@@ -69,7 +69,7 @@ const webpackBaseConfig: Configuration = {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
-              sourceMaps: !isProduction,
+              sourceMap: !isProduction,
             },
           },
           'postcss-loader',
